3607840d5770f5c78ca545f375b9aad9b2c49835
shang-jui <hb15637648@gmail.com>
2022-11-13 00:06:36 +0800

    [feat] hostname severjs

--- root/components/LoginWithGoogle.js
+++ root/components/LoginWithGoogle.js
@@ -1,14 +1,25 @@
 /** @jsxImportSource @emotion/react */
+import React, { useEffect, useState } from "react";
 import GoogleIcon from "@mui/icons-material/Google";
 import ButtonComponent from "./ButtonComponent";
+import { logInWithGoogle } from "../api/logIn";
 
 export default function LoginWithGoogle({ style }) {
+  const [googleUrl, setGoogleUrl] = useState("");
+  useEffect(() => {
+    const getGoogleUrl = async () => {
+      const res = await logInWithGoogle();
+      setGoogleUrl(res.payload.data.url);
+    };
+    getGoogleUrl();
+  }, []);
   return (
     <ButtonComponent
       variant={"outlined"}
       startIcon={<GoogleIcon />}
       style={style}
       btnText={"使用google登入"}
+      href={googleUrl}
     />
   );
 }
